c75f93e9df12c993007a91a0984aa62026430a03
MsZhaoXY <38926414+Rebeccaz0719@users.noreply.github.com>
2018-11-18 17:17:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,69 +1,13 @@
-import React from 'react'
-import './detail.css'
-import previous from '../../assets/images/previous.png'
-import next from '../../assets/images/next.png'
-import close from '../../assets/images/close.png'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-export const Detail  = (props) => {
-  const { detailShow , onPrev, onNext, onClose, ddetail} = props
-  const visibleStyle = {
-    display:detailShow?"block":"none"
-  }
-  const renderTages = (d=>{
-        if(d && d.tags.length>0){
-            return d.tags.map((t,index)=>(
-                <span key={index} className="user-tag">{t}</span>
-            ))
-        }
-    })
+ReactDOM.render(<App />, document.getElementById('root'));
 
-  return (
-    <div className="detail-container" style={visibleStyle}>
-        <div className="modal-mask"></div>
-          <div className="detail-modal">
-              <div className="modal-header">
-                  <div className="change-item">
-                      <img src={previous} alt="" className="prev-item" title="previous" onClick={onPrev}/>
-                      <img src={next} alt="" className="next-item" title="next" onClick={onNext}/>
-                  </div>
-                  <div className="modal-close">
-                      <img src={close} alt="" className="close-img" title="close" onClick={onClose}/>
-                  </div>
-              </div>
-              <div className="modal-content">
-                  <div className="modal-content-left">
-                      <img src={ddetail?ddetail.picture:""} alt=""/>
-                  </div>
-                  <div className="modal-content-right">
-                      <div className="user-info">
-                          <div>
-                              <p className="user-name">{ddetail?ddetail.user.name:""}</p>
-                              <p className="user-phone">Phone：{ddetail?ddetail.user.phone:""}</p>
-                              <p className="user-age">age: {ddetail?ddetail.user.age:""}</p>
-                              <p className="user-email">Email：{ddetail?ddetail.user.email:""}</p>
-                              <address className="user-address">Address：{ddetail?ddetail.user.address:""}</address>
-                          </div>
-                          <div className="user-tags">
-                              {renderTages(ddetail)}
-                          </div>
-                          <div>
-                              <div className="like-item">
-                                  <strong>{ddetail?ddetail.likes:0}</strong>
-                                  <span>likes</span>
-                              </div>
-                              <div className="comment-item">
-                                  <strong>{ddetail?ddetail.comments:0}</strong>
-                                  <span>comments</span>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-    </div>
-
-
-
-  )
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
